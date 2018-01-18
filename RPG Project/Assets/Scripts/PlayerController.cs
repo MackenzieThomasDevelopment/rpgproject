635ba804b95cd447413015004c4215e7a36f1e92
Mackenzie Thomas <mackenzie.thomas54@gmail.com>
2018-01-18 14:33:04 -0600

    Added a character

---
+++ RPG Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        }
+	}
+}
